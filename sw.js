2c66e78cb116a27d1ee1157cb3b95e7cab86efd0
Tal-Wa <talwallhe@gmail.com>
2026-04-26 21:03:37 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,17 @@
-// Service Worker for ShadowApp PWA
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'shadowapp-v1';
+const URLS_TO_CACHE = ['/', '/manifest.json', '/Tree-3--Streamline-Sharp.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
